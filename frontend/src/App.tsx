d8ddc53307030cec01a735703254538e4adbe7b3
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-01-31 23:17:58 +0530

    basic frontend bot

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Chatbot from './components/Chatbot';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Chatbot />
+    </div>
+  );
+};
+
+export default App;
